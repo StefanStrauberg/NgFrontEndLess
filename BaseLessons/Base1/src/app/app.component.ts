662b7ac58909776f653a625132b2caf2afd7b1a8
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-03-18 15:16:51 +0300

    Binding

--- BaseLessons/Base1/src/app/app.component.ts
+++ BaseLessons/Base1/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: []
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Dynamic title';
@@ -12,4 +12,10 @@ export class AppComponent {
   arr = [1, 2, 3];
   obj = {a: 1, b:{c: 3} };
   img = 'https://icons.iconarchive.com/icons/paomedia/small-n-flat/512/sign-delete-icon.png';
+  constructor() {
+    setTimeout(()=>{
+      console.log('timeout is over');
+      this.img = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Sign-check-icon.png/640px-Sign-check-icon.png"
+    },5000)
+  }
 }
