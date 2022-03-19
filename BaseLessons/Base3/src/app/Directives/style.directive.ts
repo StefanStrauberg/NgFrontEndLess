26f29e6caa2fcff59ccb2b00d5f19fff15547162
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-03-19 21:18:57 +0300

    HostListener

---
+++ BaseLessons/Base3/src/app/Directives/style.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, ElementRef, HostListener, Renderer2 } from "@angular/core";
+
+
+@Directive({
+    selector: '[appStyle]'
+})
+
+export class StyleDirective {
+    constructor(private el: ElementRef, private r: Renderer2) {
+        //this.r.setStyle(this.el.nativeElement, 'color', 'blue');
+        // el.nativeElement.style.color = 'RoyalBlue';
+    }
+
+    @HostListener('click', ['$event.target']) onClick(event: Event){
+        console.log(event);
+    }
+
+    @HostListener('mouseenter') onEnter(){
+        this.r.setStyle(this.el.nativeElement, 'color', 'blue');
+    }
+
+    @HostListener('mouseleave') onLeave(){
+        this.r.setStyle(this.el.nativeElement, 'color', null);
+    }
+}
--- BaseLessons/Base3/src/app/app.module.ts
+++ BaseLessons/Base3/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StyleDirective } from './Directives/style.directive';
+import { Style2Directive } from './Directives/style2.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StyleDirective,
+    Style2Directive
   ],
   imports: [
     BrowserModule,
