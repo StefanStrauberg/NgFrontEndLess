c5401b986d1e7628cb1db14cd3e94fa3b06bb944
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-03-19 22:16:01 +0300

    HostBinding

--- BaseLessons/Base3/src/app/Directives/style.directive.ts
+++ BaseLessons/Base3/src/app/Directives/style.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener, Input, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer2 } from "@angular/core";
 
 
 @Directive({
@@ -6,9 +6,10 @@ import { Directive, ElementRef, HostListener, Input, Renderer2 } from "@angular/
 })
 
 export class StyleDirective {
-    @Input('appStyle') color: string = 'black';
+    @Input('appStyle') color: any = 'black';
     @Input('fontWeight') fontWeight: string = 'normal';
-    @Input() dStyles!: {border?: string, fontWeight?: string, borderRadius?: string}
+    @Input() dStyles!: {border?: string, fontWeight?: string, borderRadius?: string};
+    @HostBinding('style.color') elColor = null;
 
     constructor(private el: ElementRef, private r: Renderer2) {
         //this.r.setStyle(this.el.nativeElement, 'color', 'blue');
@@ -20,16 +21,22 @@ export class StyleDirective {
     }
 
     @HostListener('mouseenter') onEnter(){
-        this.r.setStyle(this.el.nativeElement, 'color', this.color);
-        this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight);
-        this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border);
-        this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyles.borderRadius);
+        this.elColor = this.color;
+        //this.r.setStyle(this.el.nativeElement, 'color', this.color);
+        if(this.fontWeight != null, this.dStyles != null){
+            this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight);
+            this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border);
+            this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyles.borderRadius);
+        }
     }
 
     @HostListener('mouseleave') onLeave(){
-        this.r.setStyle(this.el.nativeElement, 'color', null);
-        this.r.setStyle(this.el.nativeElement, 'fontWeight', null);
-        this.r.setStyle(this.el.nativeElement, 'border', null);
-        this.r.setStyle(this.el.nativeElement, 'borderRadius', null);
+        this.elColor = null;
+        //this.r.setStyle(this.el.nativeElement, 'color', null);
+        if(this.fontWeight != null, this.dStyles != null){
+            this.r.setStyle(this.el.nativeElement, 'fontWeight', null);
+            this.r.setStyle(this.el.nativeElement, 'border', null);
+            this.r.setStyle(this.el.nativeElement, 'borderRadius', null);
+        }
     }
 }
--- BaseLessons/Base3/src/app/app.module.ts
+++ BaseLessons/Base3/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StyleDirective } from './Directives/style.directive';
-import { Style2Directive } from './Directives/style2.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StyleDirective,
-    Style2Directive
+    StyleDirective
   ],
   imports: [
     BrowserModule,
