b2e6ba3feb2281c8d907140590c90ea6ec0e5287
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-03-20 18:55:53 +0300

    Initial Base5 Services

---
+++ BaseLessons/Base5/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AppCounterService } from './Services/app-counter.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor(public appCounterService: AppCounterService) {}
+}
