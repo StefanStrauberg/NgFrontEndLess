cbbe050dbc0e87197d2b7a9f3df1b7be0fc1d092
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-03-21 10:16:08 +0300

    Form and Control

--- BaseLessons/Base6/src/app/app.component.ts
+++ BaseLessons/Base6/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Base6';
+export class AppComponent implements OnInit {
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(null)
+    });
+  }
+
+  submit(){
+    console.log('Form: ', this.form);
+    const formData = {... this.form.value};
+    console.log('Form Data: ', formData);
+  }
 }
