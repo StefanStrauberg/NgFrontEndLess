3c370a4657c97f280cfe8114e4f0c32922eacd63
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-03-21 16:23:52 +0300

    with bug

--- BaseLessons/Base6/src/app/app.module.ts
+++ BaseLessons/Base6/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     ReactiveFormsModule
   ],
   providers: [],
