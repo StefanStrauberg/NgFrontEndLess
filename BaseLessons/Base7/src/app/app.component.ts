846468273769b8b8210728ff23d832c98f42da26
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-03-22 14:47:31 +0300

    HttpGet date from the API

--- BaseLessons/Base7/src/app/app.component.ts
+++ BaseLessons/Base7/src/app/app.component.ts
@@ -1,4 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+export interface Todo {
+  complited: boolean,
+  title: string,
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
@@ -6,6 +13,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
+
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient){}
   ngOnInit(): void {
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/posts?_limit=2')
+      .subscribe(response => {
+        console.log('Response', response);
+        this.todos = response;
+      });
   }
 }
