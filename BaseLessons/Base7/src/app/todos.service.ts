e9069f1b0aa06aa1c6915e4f1fad57467ca8a4ba
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-03-23 12:08:53 +0300

    Interceptors in pipline REST

--- BaseLessons/Base7/src/app/todos.service.ts
+++ BaseLessons/Base7/src/app/todos.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError, delay, Observable, throwError } from "rxjs";
+import { catchError, delay, Observable, throwError, map, tap } from "rxjs";
 
 export interface Todo{
     id?: number,
@@ -12,35 +12,58 @@ export interface Todo{
 export class TodosService{
     constructor(private http: HttpClient){}
 
-    fetchTodos(): Observable<Todo[]>{
+    fetchTodos(): Observable<Todo[] | any>{
         let params = new HttpParams();
         params = params.append('_limit','4');
         params = params.append('custom', 'anything');
         return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos', {
             // params: new HttpParams().set('_limit','3')
-            params
+            params,
+            observe: 'response'
         })
-        .pipe(delay(1000),
-        catchError(error => {
-            console.log('Error: ',error.message);
-            return throwError(error);
-        })
-        );
+            .pipe(
+                map(response => {
+                    console.log('Response: ', response);
+                    return response.body;
+                }),
+                delay(1000),
+                catchError(error => {
+                    console.log('Error: ',error.message);
+                    return throwError(error);
+                })
+            );
     }
     
     addTodo(todo: Todo): Observable<Todo>{
         return this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo, {
             headers: new HttpHeaders({
-                'MyCustomHeader': Math.random().toString()
+                'MyCustomHeader': 'Alach AKbar! Fuck You! Muther Fucker!'
             })
         });
     }
 
-    removeTodo(id: number): Observable<void>{
-        return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    removeTodo(id: number): Observable<any>{
+        return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+            observe: 'events'
+        }).pipe(
+            tap(event => {
+                if(event.type === HttpEventType.Sent){
+                    console.log('Remove Event Sent: ',event);
+                }
+                if(event.type === HttpEventType.Response)
+                {
+                    console.log('Remove Event Response: ',event);
+                }
+            })
+        );
     }
 
-    completeTodo(id: number): Observable<Todo>{
-        return this.http.put<Todo>(`https://jsonplaceholder.typicode.com/todos/${id}`,{ completed: true});
+    completeTodo(id: number): Observable<Todo | any>{
+        return this.http.put<Todo>(`https://jsonplaceholder.typicode.com/todos/${id}`,{
+             completed: true
+            }, {
+                //responseType: 'json' // Default
+            }
+        );
     }
 }
