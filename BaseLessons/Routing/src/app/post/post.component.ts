952326ec499aab95ef102e13a515468ff498f4c6
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-03-23 17:10:28 +0300

    Resolves

--- BaseLessons/Routing/src/app/post/post.component.ts
+++ BaseLessons/Routing/src/app/post/post.component.ts
@@ -14,10 +14,14 @@ export class PostComponent implements OnInit {
   constructor(private route: ActivatedRoute, private postsService: PostsService, private router: Router) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => {
-      console.log('Params: ', params);
-      this.post = this.postsService.getById(+params['id'])!; //+ parse "id" to string
+    //this.post = this.route.snapshot.data['post']; // First version
+    this.route.data.subscribe(data => {
+      this.post = data['post']
     })
+    // this.route.params.subscribe((params: Params) => {
+    //   console.log('Params: ', params);
+    //   this.post = this.postsService.getById(+params['id'])!; //+ parse "id" to string
+    // })
   }
 
   loadPost(){
